307281cad054f75a9c6bdf00bd5d4fd168021007
Michael Lange <dingoeatingfuzz@gmail.com>
2020-08-08 11:12:20 -0700

    Serve on 0.0.0.0 for docker reasons

--- ui/standalone/server.js
+++ ui/standalone/server.js
@@ -34,7 +34,7 @@ fastify.get('/ui', redirect);
 
 const start = async () => {
   try {
-    await fastify.listen(3000);
+    await fastify.listen(3000, '0.0.0.0');
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (err) {
     fastify.log.error(err);
